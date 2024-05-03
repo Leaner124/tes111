7c8b0658e22fb4301e9ca7b011f38a5257d4f670
Leaner124 <168827949+Leaner124@users.noreply.github.com>
2024-05-03 22:43:26 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ function handleClick() {
   score++;
   const scoreElement = document.getElementById('score');
   scoreElement.innerText = score;
-  scoreElement.style.fontSize = '30px';
-  setTimeout(() => {
-    scoreElement.style.fontSize = '36px';
-  }, 300);
 }
 
 document.getElementById('clickButton').addEventListener('click', handleClick);
