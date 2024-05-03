7144eb475403bdb20e2fa7962aacfeabb80d09f9
Leaner124 <168827949+Leaner124@users.noreply.github.com>
2024-05-03 22:45:49 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ function handleClick() {
   scoreElement.innerText = score;
 }
 
+// Добавляем обработчик события 'touchstart' для предотвращения приближения экрана при быстрых нажатиях
+document.getElementById('clickButton').addEventListener('touchstart', function(event) {
+  event.preventDefault(); // Предотвращаем стандартное действие браузера
+  handleClick(); // Вызываем функцию обработки клика
+});
+
+// Добавляем обработчик события 'click' для обычных кликов мышью
 document.getElementById('clickButton').addEventListener('click', handleClick);
